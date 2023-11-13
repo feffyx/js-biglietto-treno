742edcc7e06b76cb5c31d164f18a9827498c3b03
feffyx <ziacofederica@gmail.com>
2023-11-13 16:36:10 +0100

    javascript completed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-
 // Chiedi informazioni
 let distance = parseFloat(prompt("Type distance in km"));
 let age = parseInt(prompt("Enter your age"));
 
 // Calcola il prezzo del biglietto
-let prezzoBase = kmDaPercorrere * 0.21;
+let prezzoBase = distance * 0.21;
 
+// Sconti
+if (age < 18) {
+    prezzoBase -= prezzoBase * 0.2; 
+} else if (age >= 65) {
+    prezzoBase -= prezzoBase * 0.4; 
+}
 
+// Prezzo
+let prezzoFinale = prezzoBase.toFixed(2);
+
+// HTML
+let risultatoElement = document.createElement("p");
+risultatoElement.textContent = "Ticket Price: " + prezzoFinale + " €";
+document.body.appendChild(risultatoElement);
