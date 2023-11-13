d0890d818b689dd2396c24d46ecbdd3032d1f76d
feffyx <ziacofederica@gmail.com>
2023-11-13 16:19:33 +0100

    added first values

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+// Chiedi informazioni
+let distance = parseFloat(prompt("Type distance in km"));
+let age = parseInt(prompt("Enter your age"));
+
+// Calcola il prezzo del biglietto
+let prezzoBase = kmDaPercorrere * 0.21;
+
+
